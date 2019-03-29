85d93a13a7a64c0b598896c205e1e778c1ea171d
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-03-29 11:03:08 +0300

    [add] buildCollage() to create and arrange layers

--- app.js
+++ app.js
@@ -7,10 +7,26 @@ const collageGenerator = (function() {
     sideSize: null
   };
 
+  const layers = [];
+
   function init(sideSize = 500, x = sideSize / 2, y = sideSize / 2) {
     settings.intersectionPoint.x = x;
     settings.intersectionPoint.y = y;
     settings.sideSize = sideSize;
+    buildCollage();
+  }
+
+  function buildCollage() {
+    const quoteLayer = collage.appendChild(document.createElement("canvas"));
+    const dimLayer = collage.appendChild(document.createElement("canvas"));
+    const imagesLayer = collage.appendChild(document.createElement("canvas"));
+
+    layers.map((layer, index) => {
+      layer.width = settings.sideSize;
+      layer.height = settings.sideSize;
+      layer.style.zIndex = index;
+      layer.style.position = "absolute";
+    });
   }
 
   return {
