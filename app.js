d146c406a268ea3e5e0f323be57b0673458fd867
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-03-29 10:50:01 +0300

    [add] settings initialization

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const collageGenerator = (function() {
-  function init() {}
+  const settings = {
+    intersectionPoint: {
+      x: null,
+      y: null
+    },
+    sideSize: null
+  };
+
+  function init(sideSize = 500, x = sideSize / 2, y = sideSize / 2) {
+    settings.intersectionPoint.x = x;
+    settings.intersectionPoint.y = y;
+    settings.sideSize = sideSize;
+  }
+
   return {
     init: init
   };
